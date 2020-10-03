fe3035ee819f17859441fa54c693e6d2229eafed
Artur Shum <ashum@stevens.edu>
2020-10-03 12:25:12 -0400

    12:25

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 // import * as request from 'request-promise-native';
 
@@ -91,20 +91,20 @@ const data = async() =>  {
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
+      <header className="App-header"> 
+        {  <img src={logo} className="App-logo" alt="logo" /> 
+        /* <p>
           Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
+        </p> */
+        /* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
-      </header>
+          Learn React */
+        /* </a>} */
+  }</header> 
     </div>
   );
 }
